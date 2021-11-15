01bc88faf32b8170ebfa3737b27571f9b9fbfe82
Anthony <ajclay7@gmail.com>
2021-11-14 20:27:19 -0500

    Add footer and make mobile nav work

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+///////////////////////////////////////////////////////////
+// Typing animation
+
 let app = document.getElementById('type');
 
 let typewriter = new Typewriter(app, {
@@ -19,3 +22,20 @@ typewriter.typeString('Programmer.')
     .typeString('Artist.')
     .pauseFor(2500)
     .start();
+
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
